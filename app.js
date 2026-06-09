5e804acd64bcda9f8349836505977d3bc6b7ae23
Kartikey_Singh <kkartikeysinghh@gmail.com>
2026-06-09 20:26:36 +0530

    create some folder to look professional, add some api-response api-error code in utils and constants in utils folder

--- app.js
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv"
-
-dotenv.config({
-  path: "./.env",
-  override: true,
-})
-
-console.log(process.env.username)
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import dotenv from "dotenv"
+dotenv.config({
+  path: "./.env",
+  override: true,
+})
+
+import app from "./app.js"
+
+
+const port = process.env.PORT || 3000
+
+
+
+
+
+
+
+
+
+
+app.listen(port, ()=>{
+  console.log(`Server is running on port ${port}`);
+  
+})
