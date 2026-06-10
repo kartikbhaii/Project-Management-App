fb029b5fcff7f119b406a001ab15f6540ab36c5f
Kartikey_Singh <kkartikeysinghh@gmail.com>
2026-06-10 20:50:51 +0530

    generating jwt access token, refresh token and using crypto module - generating temporary tokens.

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose"
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import crypto from "crypto"
+
 
 const userSchema = new mongoose.Schema(
  {
@@ -62,7 +65,7 @@ const userSchema = new mongoose.Schema(
 
 )
 
-// encrypting/hash the password before saving to mongoose so it doent save password as plain text.
+// encrypting/hash the password before saving to mongoose so it doent save password as plain text: (called "pre Hook")
 
 userSchema.pre("save", async function(next){
     if(!this.isModified("password")) return next()
@@ -75,6 +78,51 @@ userSchema.pre("save", async function(next){
 
 userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password, this.password)
+}
+
+// generating JWT ACCESS Token
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn: process.env.ACCESS_TOKEN_EXPIRY}
+    )
+}
+
+
+// generating JWT REFRESH Token 
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {expiresIn: process.env.REFRESH_TOKEN_EXPIRY}
+    )
+}
+
+
+// temporary tokens (using crypto module) use for passwordReset
+
+userSchema.methods.generateTemporaryToken = function(){
+    const unHashedToken = crypto.randomBytes(20).toString("hex")
+
+    const hashedToken = crypto
+                        .createHash("sha256")
+                        .update(unHashedToken)
+                        .digest("hex")
+
+    const tokenExpiry = Date.now() + (20*60*1000) // add 20 minutes
+
+    return {unHashedToken, hashedToken, tokenExpiry}
 }
 
 
