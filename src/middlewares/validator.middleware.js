5aa81897645a0a09fc8cecbe7d1f52e3dcc1e939
Kartikey_Singh <kkartikeysinghh@gmail.com>
2026-06-12 01:39:03 +0530

    did 3 things for express-validators package: design the middleware for validation, write validation code to check valid or not, then add to the routes

---
+++ src/middlewares/validator.middleware.js
@@ -0,0 +1,19 @@
+import { validationResult } from "express-validator";
+import { ApiError } from "../utils/api-error.js"
+
+
+// design the middleware for validation
+export const validate = (req, res, next) => {
+    const errors = validationResult(req)
+
+    if (errors.isEmpty()) {
+        return next()
+    }
+
+    const extractedErrors = errors.array().map(err => ({
+        [err.path]: err.msg
+    }))
+
+    throw new ApiError(422, "Received data is not valid", extractedErrors
+    )
+}
---
+++ src/validators/index.js
@@ -0,0 +1,38 @@
+import { body } from "express-validator"
+
+
+// writing validation code: to check if they follow the below conditions then they valid.
+const userRegisterValidator = () => {
+    return [
+        body("email")
+        .trim()
+        .notEmpty()
+        .withMessage("Email is required")
+        .isEmail()
+        .withMessage("Email is invalid"),
+
+        body("username")
+        .trim()
+        .notEmpty()
+        .withMessage("Username is required")
+        .isLowercase()
+        .withMessage("Username must be in lower case")
+        .isLength({min: 3})
+        .withMessage("Username must be at least 3 characters long"),
+
+        body("password")
+        .trim()
+        .notEmpty()
+        .withMessage("Pasword is required"),
+
+        body("fullName")
+        .optional()
+        .trim()
+    ]
+}
+
+
+
+export {
+    userRegisterValidator
+}
