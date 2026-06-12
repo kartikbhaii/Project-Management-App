bed9c59920152ea4f89ba11653efe277f3166ded
Kartikey_Singh <kkartikeysinghh@gmail.com>
2026-06-12 17:12:27 +0530

    login user with tokens and cookie data

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 import express from "express"
 import cors from "cors"
+import cookieParser from "cookie-parser"
 
 const app = express()
+
+ 
 
 // basic configurations
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
+app.use(cookieParser())
 
 
 // cors configurations - must be handled in "backend"
